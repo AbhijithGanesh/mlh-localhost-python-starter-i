0f1d98d9942a9dd8f2ec30fecc49c6ddb50cbf28
Jamie <jamie@mlh.io>
2019-03-09 09:09:19 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import json
+import config
+from markov_bot import generate_bot_answer
+from content_moderator import moderate
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+# Renders UI
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+
+# Chat API - WebSocket
+@socketio.on("send question")
+def generate_message(body, methods=["POST"]):
+    question = body["message"]
+    twitter_handle = body["username"]
+
+    try:
+        bot_answer = generate_bot_answer(twitter_handle, question)
+        moderated_bot_answer = moderate(bot_answer)
+
+        answer = {"username": twitter_handle, "message": moderated_bot_answer}
+        socketio.emit("bot answer", answer)
+    except:
+        bot_answer = "Sorry, I couldn't process that. Try again please."
+        socketio.emit("error", {"username": twitter_handle, "message": bot_answer})
+
+
+if __name__ == "__main__":
+    socketio.run(app)
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# MLH Localhost <> Capital One
+
+This project contains the starter code for the MLH Localhost Workshop Intro to Python I
+
+## File Structure
+
+- Describe Files here
+
+## Solutions
+
+You can find the solutions to each step below, if you are stuck! 
